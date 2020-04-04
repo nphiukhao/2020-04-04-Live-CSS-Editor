4e8dee2809f59cbe8392b412d73e24c259bf64f3
nphiukhao <nphiukhao@gmail.com>
2020-04-04 12:19:43 -0700

    unfinished

--- editor/src/App.js
+++ editor/src/App.js
@@ -4,37 +4,39 @@ import "./App.css";
 class App extends Component{
 
   state = {
-    title: 'color',
-    css: 'grey'
+    style: '',
   }
 
-  updateCss = (e) => {
-    e.preventDefault()
+  // updateCss = (e) => {
+  //   e.preventDefault()
 
-    console.log( e.target.value)
-    this.setState({css: e.target.value})
-    setTimeout(function() {
-      alert('changing css now!');
-    }, 3000);
-  }
+  //   console.log( e.target.value)
+  //   this.setState({css: e.target.value})
+  //   console.log(this.state)
+  // }
 
   changeCss = (e) => {
     e.preventDefault()
-    console.log( e.target.value)
-    this.setState({css: e.target.value})
+    let css = document.getElementById("text")
+    console.log(css.value)
+    // console.log( e.target.value)
+    this.setState({style: "white"})
     console.log('submitting')
+    console.log(this.state)
 
   }
+  
 
   render() {
     return (
-      <div className="App" style={{color: 'black'}}>
+      <div className="App">
+        <style dangerouslySetInnerHTML={{__html: `* {${this.state}}`}}></style>
         <header>
           <h1>Welcome to my Website</h1>
         </header>
   
         <div className="text-input">
-          <textarea onChange={e => this.updateCss(e)} placeholder="color: white;">
+          <textarea id='text' placeholder="color: white;">
 
           </textarea>
           <button type="submit" onClick={e => this.changeCss(e)}>change</button>
